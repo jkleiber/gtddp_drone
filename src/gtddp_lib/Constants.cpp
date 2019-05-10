5e748ce370ac70d47b28de5bacc21676eddae358
Justin Kleiber <jkleiber8@gmail.com>
2019-05-10 00:36:56 -0500

    Makes a successful offline trajectory. Will test in the morning

--- src/gtddp_lib/Constants.cpp
+++ src/gtddp_lib/Constants.cpp
@@ -26,11 +26,11 @@ namespace Constants {
     extern const int num_controls_v(4);
 //  
     // DDP Hyperparameters
-    extern const int num_time_steps(501);
+    extern const int num_time_steps(1001); //used to be 501
     extern const int num_iterations(15); 
     extern const int num_long_legs(2);
-    //extern const int short_iterations(3); 
-    extern const int short_iterations(15); 
+    extern const int short_iterations(7); 
+    //extern const int short_iterations(15); 
 
     // DDP Constant doubles
     // Change as necessary, do not remove
