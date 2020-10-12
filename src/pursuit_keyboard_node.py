f8753bd4361678d83fb7c9d91f2b03cdbfe7a422
Justin Kleiber <jkleiber8@gmail.com>
2020-10-12 00:15:31 -0400

    Upgrades the pursuit keyboard to have double landings and specific drone controls

--- src/pursuit_keyboard_node.py
+++ src/pursuit_keyboard_node.py
@@ -37,6 +37,57 @@ def getKey(settings):
     termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
     return key
 
+def manual_control(key, reset_pub, takeoff_pub, manual_ctrl_pub):
+    # R is for reset. Reset a crashed drone without a power cycle
+    if (key == 'r'):
+        reset_msg = Empty()
+        reset_pub.publish(reset_msg)
+    elif (key == 't'):
+        takeoff_msg = Empty()
+        takeoff_pub.publish(takeoff_msg)
+    # W commands a positive vertical speed
+    elif (key == 'a'):
+        vel.linear.x = 0
+        vel.linear.y = 0
+        vel.linear.z = SPEED
+        manual_ctrl_pub.publish(vel)
+    # S commands a negative vertical speed
+    elif (key == 'z'):
+        vel.linear.x = 0
+        vel.linear.y = 0
+        vel.linear.z = -SPEED
+        manual_ctrl_pub.publish(vel)
+    # C commands a positive pitch
+    elif (key == 'c'):
+        vel.linear.x = SPEED
+        vel.linear.y = 0
+        vel.linear.z = 0
+        manual_ctrl_pub.publish(vel)
+    # X commands a negative pitch
+    elif (key == 'x'):
+        vel.linear.x = -SPEED
+        vel.linear.y = 0
+        vel.linear.z = 0
+        manual_ctrl_pub.publish(vel)
+    # E commands a positive roll
+    elif (key == 'e'):
+        vel.linear.x = 0
+        vel.linear.y = SPEED
+        vel.linear.z = 0
+        manual_ctrl_pub.publish(vel)
+    # D commands a negative roll
+    elif (key == 'd'):
+        vel.linear.x = 0
+        vel.linear.y = -SPEED
+        vel.linear.z = 0
+        manual_ctrl_pub.publish(vel)
+    # If no command is pressed, then make sure not to affect the drone
+    elif (vel.linear.z != 0 or vel.linear.x != 0 or vel.linear.y != 0):
+        vel.linear.z = 0
+        vel.linear.x = 0
+        vel.linear.y = 0
+        manual_ctrl_pub.publish(vel)
+
 
 vel = Twist()
 SPEED = 0.3
@@ -48,14 +99,22 @@ if __name__=="__main__":
 
     # Set up ROS publishers
     # Drone 1
-    takeoff_pub = rospy.Publisher('/ardrone/takeoff', Empty, queue_size=10)
-    land_pub = rospy.Publisher('/ardrone/land', Empty, queue_size=10)       # emergency land the drone
-    reset_pub = rospy.Publisher('/ardrone/reset', Empty, queue_size=10)     # Reset a crashed drone
-    manual_ctrl_pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10)     # Manual control
+    takeoff_pub = rospy.Publisher('/drone1/ardrone/takeoff', Empty, queue_size=10)
+    land_pub = rospy.Publisher('/drone1/ardrone/land', Empty, queue_size=10)       # emergency land the drone
+    reset_pub = rospy.Publisher('/drone1/ardrone/reset', Empty, queue_size=10)     # Reset a crashed drone
+    manual_ctrl_pub = rospy.Publisher('/drone1/cmd_vel', Twist, queue_size=10)     # Manual control
+
+    # Drone 2
+    takeoff_pub_2 = rospy.Publisher('/drone2/ardrone/takeoff', Empty, queue_size=10)
+    land_pub_2 = rospy.Publisher('/drone2/ardrone/land', Empty, queue_size=10)       # emergency land the drone
+    reset_pub_2 = rospy.Publisher('/drone2/ardrone/reset', Empty, queue_size=10)     # Reset a crashed drone
+    manual_ctrl_pub_2 = rospy.Publisher('/drone2/cmd_vel', Twist, queue_size=10)     # Manual control
 
     # Both Drones
     init_pub = rospy.Publisher('/gtddp_drone/start', Empty, queue_size=10)  # GO button
 
+    # Selected drone
+    active_drone = 1
 
     # Initialize the ROS node
     rospy.init_node('keyboard_node')
@@ -73,65 +132,27 @@ if __name__=="__main__":
             termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
 
         # print(key)
-        # If the key is the E-Stop key, land the drone
-        if (key == ' '):
+        if(key == '1'):
+            active_drone = 1
+        elif(key == '2'):
+            active_drone = 2
+        # If the key is the E-Stop key, land the drones
+        elif (key == ' '):
             land_msg = Empty()
             land_pub.publish(land_msg)
+            land_pub_2.publish(land_msg)
         # G is for go. Initialize the optimizer
         elif (key == 'g'):
             init_msg = Empty()
             init_pub.publish(init_msg)
-        # R is for reset. Reset a crashed drone without a power cycle
-        elif (key == 'r'):
-            reset_msg = Empty()
-            reset_pub.publish(reset_msg)
-        elif (key == 't'):
-            takeoff_msg = Empty()
-            takeoff_pub.publish(takeoff_msg)
-        # W commands a positive vertical speed
-        elif (key == 'a'):
-            vel.linear.x = 0
-            vel.linear.y = 0
-            vel.linear.z = SPEED
-            manual_ctrl_pub.publish(vel)
-        # S commands a negative vertical speed
-        elif (key == 'z'):
-            vel.linear.x = 0
-            vel.linear.y = 0
-            vel.linear.z = -SPEED
-            manual_ctrl_pub.publish(vel)
-        # C commands a positive pitch
-        elif (key == 'c'):
-            vel.linear.x = SPEED
-            vel.linear.y = 0
-            vel.linear.z = 0
-            manual_ctrl_pub.publish(vel)
-        # X commands a negative pitch
-        elif (key == 'x'):
-            vel.linear.x = -SPEED
-            vel.linear.y = 0
-            vel.linear.z = 0
-            manual_ctrl_pub.publish(vel)
-        # E commands a positive roll
-        elif (key == 'e'):
-            vel.linear.x = 0
-            vel.linear.y = SPEED
-            vel.linear.z = 0
-            manual_ctrl_pub.publish(vel)
-        # D commands a negative roll
-        elif (key == 'd'):
-            vel.linear.x = 0
-            vel.linear.y = -SPEED
-            vel.linear.z = 0
-            manual_ctrl_pub.publish(vel)
         # If the key is one of the exit keys, close the program
         elif (key in exitKeys):
             break
-        # If no command is pressed, then make sure not to affect the drone
-        elif (vel.linear.z != 0 or vel.linear.x != 0 or vel.linear.y != 0):
-            vel.linear.z = 0
-            vel.linear.x = 0
-            vel.linear.y = 0
-            manual_ctrl_pub.publish(vel)
+
+        # Control each drone individually based on which one is active
+        if active_drone == 1:
+            manual_control(key, reset_pub, takeoff_pub, manual_ctrl_pub)
+        elif active_drone == 2:
+            manual_control(key, reset_pub_2, takeoff_pub_2, manual_ctrl_pub_2)
 
         key = ""
